a062140096e595644a1d1974963b0ec4c526e648
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2020-06-23 10:55:01 +0200

    Fix error messages on mobile

--- app/models/brainstorm.rb
+++ app/models/brainstorm.rb
@@ -2,7 +2,7 @@ class Brainstorm < ApplicationRecord
   has_many :ideas
   attr_accessor :name
 
-  validates :problem, presence: { message: "You need to type in a problem to solve. Hit TIPS & TRICKS for help" }
+  validates :problem, presence: { message: "You need to type in a problem to solve." }
 
-  validates :name, presence: { message: "Please let the other participants know who you are"}
+  validates :name, presence: { message: "Please let the other participants know who you are."}
 end
