dd3b093ccc1abf82417872e02b56977f20ace5a2
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2020-11-21 12:10:46 +0100

    extract view change logic to separate file

---
+++ app/javascript/packs/change_view_based_on_state.js
@@ -0,0 +1,60 @@
+let stateConfiguration = {
+  setup: {
+    setup: true,
+    setup_facilitator: false,
+    setup_participant: false,
+    ideation: false,
+    time_is_up: false,
+    vote: false,
+    voting_done: false
+  },
+  ideation: {
+    setup: false,
+    setup_facilitator: false,
+    setup_participant: false,
+    ideation: true,
+    time_is_up: false,
+    vote: false,
+    voting_done: false
+  },
+  time_is_up: {
+    setup: false,
+    setup_facilitator: false,
+    setup_participant: false,
+    ideation: false,
+    time_is_up: true,
+    vote: false,
+    voting_done: false
+  },
+  vote: {
+    setup: false,
+    setup_facilitator: false,
+    setup_participant: false,
+    ideation: false,
+    time_is_up: false,
+    vote: true,
+    voting_done: false
+  },
+  voting_done: {
+    setup: false,
+    setup_facilitator: false,
+    setup_participant: false,
+    ideation: false,
+    time_is_up: false,
+    vote: false,
+    voting_done: true
+  }
+}
+
+changeView = function (state) {
+  configuration = stateConfiguration[state]
+  for (const [key, value] of Object.entries(configuration)) {
+    document.getElementById(key).style.display = value ? 'block' : 'none';
+  }
+
+  if (typeof brainstormStore.state === 'undefined' || brainstormStore.state === 'setup') {
+
+    document.querySelector("#setup_participant").style.display = currentUser.facilitator == "false" ? "block" : "none"
+    document.querySelector("#setup_facilitator").style.display = currentUser.facilitator == "true" ? "block" : "none"
+  }
+}
