19eaf417099499b44e7131f9e421e0769a34fd10
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2021-03-30 18:15:28 +0200

    change back old number method

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -5,10 +5,24 @@ class Idea < ApplicationRecord
   validates :text, presence: {message: "You forgot to write an idea!"}
 
   def number
-    brainstorm.ideas.index(self) + 1
+    brainstorm_idea_number[self.id]
   end
 
   def vote_in_plural_or_singular
     votes > 1 || votes == 0 ? "votes" : "vote"
+  end
+
+  private
+
+  def brainstorm_idea_number
+    Hash[idea_id_range.zip number_id_range]
+  end
+
+  def idea_id_range
+    (brainstorm.ideas.first.id..brainstorm.ideas.last.id)
+  end
+
+  def number_id_range
+    (1..idea_id_range.count)
   end
 end
