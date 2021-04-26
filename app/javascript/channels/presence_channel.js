505862afa3ef108cdc75aacb8113e6c5a20e8805
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2021-04-26 22:49:46 +0200

    Show bigger nav bar on mobile

--- app/javascript/channels/presence_channel.js
+++ app/javascript/channels/presence_channel.js
@@ -92,7 +92,9 @@ consumer.subscriptions.create({
 
 const clearNameListElement = () => {
   const nameListElement = document.getElementById("name-list");
+  const nameListMobileElement = document.getElementById("name-list-mobile");
   nameListElement.innerHTML = "";
+  nameListMobileElement.innerHTML = "";
 }
 
 const clearListOfParticipants = () => {
@@ -120,7 +122,17 @@ const createListOfParticipants = (data) => {
   }
 }
 
+const createUserBadgePhone = (data) => {
+  const nameListElementMobile = document.getElementById("name-list-mobile");
+  let userBadgeMobile = document.createElement("div");
+  userBadgeMobile.classList.add("text-6xl", "font-black", "flex", "items-center");
+  let svg = '<svg class="mr-2 h-full w-16" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><rect fill="none" height="24" width="24"/></g><g><g/><g><g><path d="M16.67,13.13C18.04,14.06,19,15.32,19,17v3h4v-3 C23,14.82,19.43,13.53,16.67,13.13z" fill-rule="evenodd"/></g><g><circle cx="9" cy="8" fill-rule="evenodd" r="4"/></g><g><path d="M15,12c2.21,0,4-1.79,4-4c0-2.21-1.79-4-4-4c-0.47,0-0.91,0.1-1.33,0.24 C14.5,5.27,15,6.58,15,8s-0.5,2.73-1.33,3.76C14.09,11.9,14.53,12,15,12z" fill-rule="evenodd"/></g><g><path d="M9,13c-2.67,0-8,1.34-8,4v3h16v-3C17,14.34,11.67,13,9,13z" fill-rule="evenodd"/></g></g></g></svg>'
+  userBadgeMobile.innerHTML = svg + `<div>${data.initials.length}</div>` 
+  nameListElementMobile.appendChild(userBadgeMobile);
+}
+
 const createUserBadges = (data) => {
+  createUserBadgePhone(data);
   const nameListElement = document.getElementById("name-list");
   let nameListContainer = document.getElementById("name-list-container");
   let doneDivContainer;
@@ -132,7 +144,7 @@ const createUserBadges = (data) => {
     doneDivContainer = document.getElementById("doneDivContainer")
   }
 
-  doneDivContainer.classList.add("flex")
+  doneDivContainer.classList.add("lg:flex", "hidden")
   doneDivContainer.setAttribute("id", "doneDivContainer");
 
   doneDivContainer.innerHTML = '';
