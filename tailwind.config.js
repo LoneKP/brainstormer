5a084bdaf930825299c07aef8d3a2d19a06fc7e9
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2021-05-16 13:22:12 +0200

    remove tailwindcss ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -78,10 +78,4 @@ module.exports = {
       })
     },
   },
-  plugins: [
-    require('@tailwindcss/ui'),
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/aspect-ratio"),
-  ]
 }
