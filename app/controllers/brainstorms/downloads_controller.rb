c2deb35774dd785925aa72f70e68d329d4434c57
Kasper Timm Hansen <kaspth@gmail.com>
2021-08-03 23:20:25 +0200

    Wups, need to specify via render

--- app/controllers/brainstorms/downloads_controller.rb
+++ app/controllers/brainstorms/downloads_controller.rb
@@ -1,10 +1,8 @@
 class Brainstorms::DownloadsController < ApplicationController
-  layout "pdf"
-
   def show
     @brainstorm = Brainstorm.find_by(token: params[:token])
 
     ahoy.track "Download pdf"
-    render pdf: "Brainstorm session ideas", page_size: "A4", lowquality: true, dpi: 75
+    render pdf: "Brainstorm session ideas", layout: "pdf.html", page_size: "A4", lowquality: true, dpi: 75
   end
 end
