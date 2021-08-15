3b139212c693d381ab59ed9b1b1446cbb3f1d0ac
Kasper Timm Hansen <kaspth@gmail.com>
2021-08-15 21:29:22 +0200

    Add back set_session method for sessions/names controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
 
   private
-  
+
+  def set_session
+    set_session_id
+    @session = Session.new(@session_id)
+  end
+
   def set_session_id
     @session_id = cookies[:user_id] ||= SecureRandom.uuid
   end
-  
+
 end
-  
