f5c438d59b365849a9a46941964a8fcaeaaf4784
Kasper Timm Hansen <kaspth@gmail.com>
2021-08-15 21:40:07 +0200

    Add basic brainstorms rendering test to ferret out easy bugs

---
+++ test/controllers/brainstorms_controller_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class BrainstormsControllerTest < ActionDispatch::IntegrationTest
+  test "show" do
+    @brainstorm = Brainstorm.create! name: "Lone", problem: "Too few good ideas"
+
+    get brainstorm_path(@brainstorm.token)
+    assert_response :success
+  end
+end
