6fbcbc30a50d5bcaf6bb99d6ec96fe0f7cd7c0a3
Kasper Timm Hansen <kaspth@gmail.com>
2021-08-17 00:13:59 +0200

    Extract core variable so callsites are less busy

--- app/channels/timer_channel.rb
+++ app/channels/timer_channel.rb
@@ -2,6 +2,8 @@ class TimerChannel < ApplicationCable::Channel
 
   def subscribed
     @brainstorm = Brainstorm.find_by(token: params[:token])
+    @timer = @brainstorm.timer
+
     stream_from "brainstorm-#{params[:token]}-timer"
     update_redis_if_time_has_run_out
     transmit_list!
@@ -13,20 +15,20 @@ class TimerChannel < ApplicationCable::Channel
     ActionCable.server.broadcast("brainstorm-#{@brainstorm.token}-timer", {
       event: "transmit_timer_status",
       timer_status: timer_status,
-      brainstorm_duration: @brainstorm.timer.duration
+      brainstorm_duration: @timer.duration
     })
   end
 
   def timer_status
     case
-    when @brainstorm.timer.expired? then "time_has_run_out"
-    when @brainstorm.timer.ready?   then "ready_to_start_timer"
+    when @timer.expired? then "time_has_run_out"
+    when @timer.ready?   then "ready_to_start_timer"
     else
-      @brainstorm.timer.elapsed_seconds
+      @timer.elapsed_seconds
     end
   end
 
   def update_redis_if_time_has_run_out
-    @brainstorm.state = :vote if @brainstorm.state.ideation? && @brainstorm.timer.expired?
+    @brainstorm.state = :vote if @brainstorm.state.ideation? && @timer.expired?
   end
 end
