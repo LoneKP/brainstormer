9fbb943f373887b43378e1e42525b5d5a6594723
Kasper Timm Hansen <kaspth@gmail.com>
2021-08-18 02:41:50 +0200

    Just as clear inline

--- app/channels/timer_channel.rb
+++ app/channels/timer_channel.rb
@@ -2,17 +2,11 @@ class TimerChannel < ApplicationCable::Channel
 
   def subscribed
     stream_or_reject_for @brainstorm = Brainstorm.find_by(token: params[:token])
-    @timer = @brainstorm.timer
-    transmit_list!
-  end
 
-  private
-
-  def transmit_list!
     broadcast_to @brainstorm, {
       event: "transmit_timer_status",
-      timer_status: @timer.elapsed_seconds,
-      brainstorm_duration: @timer.duration
+      timer_status: @brainstorm.timer.elapsed_seconds,
+      brainstorm_duration: @brainstorm.timer.duration
     }
   end
 end
