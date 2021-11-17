322fa42627e3700cff5883de243a42944736a8df
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2021-11-17 20:35:38 +0100

    helper to detect device

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def device
+    agent = request.user_agent
+    return "tablet" if agent =~ /(tablet|ipad)|(android(?!.*mobile))/i
+    return "mobile" if agent =~ /Mobile/
+    return "desktop"
+  end
 end
