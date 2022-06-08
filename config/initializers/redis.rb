2d0c56ec37d75d16c7505eed7d138c8704f674fe
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2022-06-08 23:02:49 +0200

    try allowing origins for redis to work

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,6 @@
 if ENV['REDIS_URL'].present?
-  puts "redis url is present: #{ENV['REDIS_URL']}"
   uri = URI.parse(ENV['REDIS_URL'])
   REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
 else
-  puts "redis url is not present"
   REDIS = Redis.new
 end
