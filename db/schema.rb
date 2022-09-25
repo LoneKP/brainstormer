a1f0228238a7f075033d3854ed835c6401c922f1
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2022-09-25 12:45:13 +0200

    rollback db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_24_134034) do
+ActiveRecord::Schema.define(version: 2021_09_05_074428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -164,13 +164,6 @@ ActiveRecord::Schema.define(version: 2022_09_24_134034) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "votes", default: 0
     t.index ["brainstorm_id"], name: "index_ideas_on_brainstorm_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
