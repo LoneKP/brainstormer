a39c08d28bc63d423a1779ea20bbd1fa1102b534
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2022-12-28 15:32:21 +0100

    upgrade rails to 6.1.7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_13_164923) do
+ActiveRecord::Schema.define(version: 2022_12_28_142045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
