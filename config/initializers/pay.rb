4504a89773f65b33ba495f36d786ba45288f190c
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2023-02-08 21:06:15 +0100

    Update receipt email for purchase

--- config/initializers/pay.rb
+++ config/initializers/pay.rb
@@ -1,3 +1,5 @@
 Pay.setup do |config|
   config.support_email = "Brainstormer <brainstormer.online@gmail.com>"
+  config.application_name = "Brainstormer"
+  config.business_name = "Brainstormer"
 end
