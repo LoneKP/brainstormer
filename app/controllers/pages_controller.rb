a5160275152fa8746d88d430dd79c06e10fb857e
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2023-04-15 18:45:09 +0200

    Optimizations for mobile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,7 +10,7 @@ class PagesController < ApplicationController
   end
 
   def your_brainstorms
-    @most_recent_brainstorms = current_user.brainstorms.last(5)
+    @most_recent_brainstorms = current_user.brainstorms.last(5).reverse
     @ready_for_ideation = current_user.brainstorms.select(&:ready_for_ideation)
     @in_progress = current_user.brainstorms.select(&:in_progress)
     @done = current_user.brainstorms.select(&:done)
