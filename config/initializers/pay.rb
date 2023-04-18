9d7e3415258a08f94d0dd72f309da60d8302d4a6
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2023-04-18 19:25:38 +0200

    Add email updates

--- config/initializers/pay.rb
+++ config/initializers/pay.rb
@@ -2,4 +2,14 @@ Pay.setup do |config|
   config.support_email = "Brainstormer <brainstormer.online@gmail.com>"
   config.application_name = "Brainstormer"
   config.business_name = "Brainstormer"
+
+
+  config.emails.subscription_renewing = ->(pay_subscription, price) {
+    (price&.type == "recurring") && (price.recurring&.interval == "year")
+  }
+
+  config.emails.subscription_renewing = ->(pay_subscription, price) {
+    (price&.type == "recurring") && (price.recurring&.interval == "month")
+  }
+
 end
