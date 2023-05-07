0db317ba72e4298c0bbab41f97874e5d786912b8
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2023-05-07 14:10:09 +0200

    add missing commas

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,9 +76,9 @@ Rails.application.configure do
   # enable_starttls_auto: true }
 
   config.action_mailer.smtp_settings = {
-  address:              ENV['MAILGUN_SMTP_SERVER']
+  address:              ENV['MAILGUN_SMTP_SERVER'],
   port:                 ENV['MAILGUN_SMTP_PORT'],
-  domain:               ENV['MAILGUN_DOMAIN']
+  domain:               ENV['MAILGUN_DOMAIN'],
   user_name:            ENV['MAILGUN_SMTP_LOGIN'],
   password:             ENV['MAILGUN_SMTP_PASSWORD'],
   authentication:       'plain',
