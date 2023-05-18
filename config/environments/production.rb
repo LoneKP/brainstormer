ff37bc7fdc6e212c4292d9955d7763c3ee84a7dd
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2023-05-18 16:09:00 +0200

    use sendgrid to send emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,12 +75,21 @@ Rails.application.configure do
   # authentication:       'plain',
   # enable_starttls_auto: true }
 
+  # config.action_mailer.smtp_settings = {
+  # address:              ENV['MAILGUN_SMTP_SERVER'],
+  # port:                 ENV['MAILGUN_SMTP_PORT'],
+  # domain:               ENV['MAILGUN_DOMAIN'],
+  # user_name:            ENV['MAILGUN_SMTP_LOGIN'],
+  # password:             ENV['MAILGUN_SMTP_PASSWORD'],
+  # authentication:       'plain',
+  # enable_starttls_auto: true }
+
   config.action_mailer.smtp_settings = {
-  address:              ENV['MAILGUN_SMTP_SERVER'],
-  port:                 ENV['MAILGUN_SMTP_PORT'],
-  domain:               ENV['MAILGUN_DOMAIN'],
-  user_name:            ENV['MAILGUN_SMTP_LOGIN'],
-  password:             ENV['MAILGUN_SMTP_PASSWORD'],
+  user_name:            "apikey",
+  password:             ENV['SENDGRID_API_KEY'],
+  domain:               ENV['SENDGRID_DOMAIN'],
+  address:              ENV['SENDGRID_SMTP_SERVER'],
+  port:                 ENV['SENDGRID_SMTP_PORT'],
   authentication:       'plain',
   enable_starttls_auto: true }
 
