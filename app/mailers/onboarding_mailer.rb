cfebcaf6e0f690900a5757e45fbc52aebe33c89e
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2023-05-20 10:37:29 +0200

    add emojis and category to welcome email

--- app/mailers/onboarding_mailer.rb
+++ app/mailers/onboarding_mailer.rb
@@ -1,6 +1,7 @@
 class OnboardingMailer < ApplicationMailer
 
   def welcome_email
-    mail(to: params[:email_address], subject: "A personal hello" )
+    mail(to: params[:email_address], subject: "Let's get started with Brainstormer 💡" )
+    mail['X-SMTPAPI'] = { 'category' => 'welcome email' }.to_json
   end
 end
