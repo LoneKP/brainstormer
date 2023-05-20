8cbc81b0faad1a73c04ea1b2a30502fb8f45cb9f
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2023-05-20 10:47:50 +0200

    update the category header

--- app/mailers/onboarding_mailer.rb
+++ app/mailers/onboarding_mailer.rb
@@ -1,7 +1,7 @@
 class OnboardingMailer < ApplicationMailer
 
   def welcome_email
+    headers['X-MT-Category'] = 'welcome email'
     mail(to: params[:email_address], subject: "Let's get started with Brainstormer 💡" )
-    mail['X-SMTPAPI'] = { 'category' => 'welcome email' }.to_json
   end
 end
