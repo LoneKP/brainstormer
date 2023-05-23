2c01d9eeeddf1b47d41fc85178e7ef0fa9d2f9ab
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2023-05-23 17:50:16 +0200

    Add category to ideas mailer

--- app/mailers/ideas_mailer.rb
+++ app/mailers/ideas_mailer.rb
@@ -3,6 +3,7 @@ class IdeasMailer < ApplicationMailer
   def ideas_email
     @brainstorm = Brainstorm.find_by(token: params[:token])
     @ideas = @brainstorm.ideas.order(votes: :desc)
+    headers['X-MT-Category'] = 'ideas email'
     mail(to: params[:email_address], subject: 'Result from your ideation session' )
   end
 end
