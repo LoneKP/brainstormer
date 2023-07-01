bb808341a794554c6da1e02065feba769b2e371b
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2023-07-01 19:18:48 +0200

    update session class to persist name on refresh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,8 +24,7 @@ class ApplicationController < ActionController::Base
   def set_session_for_all_types
     set_visitor_id
     set_current_guest
-    set_name
-    @session = Session.new(@visitor_id, @current_guest&.id, current_user&.id, @name)
+    @session ||= Session.new(@visitor_id, @current_guest&.id, current_user&.id)
   end
 
   def set_visitor_id
@@ -36,17 +35,6 @@ class ApplicationController < ActionController::Base
     if !current_user
       @current_guest ||= Guest.find(session[:guest_id]) if session[:guest_id]
     end
-  end
-
-  def set_name
-    @name ||= current_user&.name.presence || @current_guest&.name.presence || set_random_name
-  end
-
-  def set_random_name
-    adjectives = ["Sleeping", "Hungry", "Purple", "Hairy", "Wet", "Hungarian", "Australian", "Cross-eyed", "Mind-reading", "Fierce", "Wig-wearing", "Busy", "Bingo-playing", "Talking", "Cool", "Amazing", "Happy", "Lovely", "Tomb Raiding", "Self-aware", "Flying", "Bald", "The Best", "Space-exploring", "To-the-point", "Forgetful", "Visionary", "Well-behaved", "Totally Awesome", "Hilarious", "Charming", "Royal", "Rapid", "Adorable", "Cheerful", "Expert", "Wacky", "Fluffy", "Smart", "Well-articulated", "Sophisticated", "Tickly", "Wise", "Lucky", "The One True", "Unreal", "Silly", "Smooth", "Tongue-twisting", "Tiny", "Carnivorous", "Vegetarian", "Grounded", "Mischievous", "Tropical", "Arctic", "Polite", "Frivolous", "Tactical", "Organized", "Dazzled", "Jolly", "Diligent", "Famous", "Hip", "Bored", "Unmarried", "Travelling", "Brainy", "Blue-eyed", "Nosey", "Adventurous", "Acidic", "Important"]
-    nouns = ["Tiger", "Turtle", "Cat", "Sloth", "Hedgehog", "Space Explorer", "Mouse", "Hamster", "Tea Pot", "Alien", "Traveller", "Bear", "Fish", "Donkey", "Horse", "Zebra", "Lion", "Otter", "Spider", "Architect", "Surgeon", "Friend", "Coffee Cup", "Computer", "Super Hero", "Boy", "Girl", "Ant", "Dinosaur", "Bird", "Camel", "Duck", "Gecko", "Astronaut", "Biologist", "Villain", "Body Builder", "Gummy Bear", "Ice Cream", "Ferret", "Fox", "Alpaca", "Wallaby", "Squid", "Beatle", "Beet", "Dog", "Snail", "Crab", "Camel", "Student", "Farmer", "Sheep", "Grandma", "Piggy", "Tarantula", "Hawk", "Crocodile", "Guitarist", "Drummer", "Organist", "Priest", "Hyena", "Marshmellow", "Rock", "Therapist", "Musician", "Penguin", "Piranha", "Duckling", "Puppy", "Toaster", "Barista", "Nephew", "Hippo", "Hipster", "Panda", "Bridesmaid", "Bat", "Suitcase", "Radiator", "Flashlight", "Escalator", "Elevator"]
-
-    "#{adjectives.sample} #{nouns.sample}"
   end
 
   def set_portal_session
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,8 +1,9 @@
 class Session
   attr_reader :visitor_id, :guest_id, :user_id, :name
 
-  def initialize(visitor_id, guest_id=nil, user_id=nil, name)
-    @visitor_id, @guest_id, @user_id, @name = visitor_id, guest_id, user_id, name
+  def initialize(visitor_id, guest_id=nil, user_id=nil)
+    @visitor_id, @guest_id, @user_id = visitor_id, guest_id, user_id
+    
     set_hash
   end
 
@@ -12,8 +13,38 @@ class Session
       "visitor_id" => visitor_id.to_s, 
       "guest_id" => guest_id.to_s, 
       "user_id" => user_id.to_s, 
-      "name" => name.to_s
+      "name" => set_name
     )  
+  end
+
+  def set_name
+    if user_id
+      @name = User.find(user_id).name
+    elsif guest_id
+      @name = Guest.find(guest_id).name
+    else 
+      @name = generate_or_retrieve_name(visitor_id)
+    end
+  end
+
+  def generate_or_retrieve_name(visitor_id)
+    stored_name = retrieve_name_from_storage(visitor_id)
+    return stored_name if stored_name
+
+    generated_name = generate_name
+    @name = generated_name
+    generated_name
+  end
+
+  def retrieve_name_from_storage(visitor_id)
+     @key["name"]
+  end
+
+  def generate_name
+    adjectives = ["Sleeping", "Hungry", "Purple", "Hairy", "Wet", "Hungarian", "Australian", "Cross-eyed", "Mind-reading", "Fierce", "Wig-wearing", "Busy", "Bingo-playing", "Talking", "Cool", "Amazing", "Happy", "Lovely", "Tomb Raiding", "Self-aware", "Flying", "Bald", "The Best", "Space-exploring", "To-the-point", "Forgetful", "Visionary", "Well-behaved", "Totally Awesome", "Hilarious", "Charming", "Royal", "Rapid", "Adorable", "Cheerful", "Expert", "Wacky", "Fluffy", "Smart", "Well-articulated", "Sophisticated", "Tickly", "Wise", "Lucky", "The One True", "Unreal", "Silly", "Smooth", "Tongue-twisting", "Tiny", "Carnivorous", "Vegetarian", "Grounded", "Mischievous", "Tropical", "Arctic", "Polite", "Frivolous", "Tactical", "Organized", "Dazzled", "Jolly", "Diligent", "Famous", "Hip", "Bored", "Unmarried", "Travelling", "Brainy", "Blue-eyed", "Nosey", "Adventurous", "Acidic", "Important"]
+    nouns = ["Tiger", "Turtle", "Cat", "Sloth", "Hedgehog", "Space Explorer", "Mouse", "Hamster", "Tea Pot", "Alien", "Traveller", "Bear", "Fish", "Donkey", "Horse", "Zebra", "Lion", "Otter", "Spider", "Architect", "Surgeon", "Friend", "Coffee Cup", "Computer", "Super Hero", "Boy", "Girl", "Ant", "Dinosaur", "Bird", "Camel", "Duck", "Gecko", "Astronaut", "Biologist", "Villain", "Body Builder", "Gummy Bear", "Ice Cream", "Ferret", "Fox", "Alpaca", "Wallaby", "Squid", "Beatle", "Beet", "Dog", "Snail", "Crab", "Camel", "Student", "Farmer", "Sheep", "Grandma", "Piggy", "Tarantula", "Hawk", "Crocodile", "Guitarist", "Drummer", "Organist", "Priest", "Hyena", "Marshmellow", "Rock", "Therapist", "Musician", "Penguin", "Piranha", "Duckling", "Puppy", "Toaster", "Barista", "Nephew", "Hippo", "Hipster", "Panda", "Bridesmaid", "Bat", "Suitcase", "Radiator", "Flashlight", "Escalator", "Elevator"]
+  
+    "#{adjectives.sample} #{nouns.sample}"
   end
 
   def id
