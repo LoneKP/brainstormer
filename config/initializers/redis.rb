1edc8e58945b03ea8188a545c550f84eb9f2257b
Lone Kjeldgaard <lonekjeldgaard@Mac.penneo.com>
2024-11-19 16:24:06 +0100

    add url for redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,7 @@
 if ENV['REDIS_URL'].present?
   uri = URI.parse(ENV['REDIS_URL'])
   REDIS = Redis.new(
-    :host => uri.host, 
-    :port => uri.port, 
-    :password => uri.password,
+    :url => ENV['REDIS_URL']
     :ssl => true,
     :ssl_params => { 
       :verify_mode => OpenSSL::SSL::VERIFY_NONE 
