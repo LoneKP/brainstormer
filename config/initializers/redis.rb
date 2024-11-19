c61485ca42e9588d16efc3e54f8f46a950b87eff
Lone Kjeldgaard <lonekjeldgaard@Mac.penneo.com>
2024-11-19 16:27:40 +0100

    fix syntax error

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,7 @@
 if ENV['REDIS_URL'].present?
   uri = URI.parse(ENV['REDIS_URL'])
   REDIS = Redis.new(
-    :url => ENV['REDIS_URL']
+    :url => ENV['REDIS_URL'],
     :ssl => true,
     :ssl_params => { 
       :verify_mode => OpenSSL::SSL::VERIFY_NONE 
