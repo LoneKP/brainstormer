e428859042724fffaf9c105490da6afbc62e7c14
Lone Kjeldgaard <lonekjeldgaard@Mac.penneo.com>
2024-11-19 15:47:52 +0100

    add ssl true to redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,11 @@
 if ENV['REDIS_URL'].present?
   uri = URI.parse(ENV['REDIS_URL'])
-  REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+  REDIS = Redis.new(
+    host: uri.host, 
+    port: uri.port, 
+    password: uri.password,
+    ssl: true
+  )
 else
   REDIS = Redis.new
 end
