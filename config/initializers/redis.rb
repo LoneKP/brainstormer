4ea368af3374d6a365f6b648887ed82dcf682975
Lone Kjeldgaard <lonekjeldgaard@Mac.penneo.com>
2024-11-19 16:13:49 +0100

    add different formatting

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,11 +1,13 @@
 if ENV['REDIS_URL'].present?
   uri = URI.parse(ENV['REDIS_URL'])
   REDIS = Redis.new(
-    host: uri.host, 
-    port: uri.port, 
-    password: uri.password,
-    ssl: true,
-    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+    :host => uri.host, 
+    :port => uri.port, 
+    :password => uri.password,
+    :ssl => true,
+    :ssl_params => { 
+      :verify_mode => OpenSSL::SSL::VERIFY_NONE 
+    }
   )
 else
   REDIS = Redis.new
