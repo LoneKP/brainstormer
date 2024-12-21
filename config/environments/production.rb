e038717831c54e5e585fb84ce4669984568a5ec0
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2024-12-21 14:11:38 +0100

    update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :google
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -77,6 +77,24 @@ Rails.application.configure do
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    user_name:            ENV['MAILTRAP_USER_NAME'],
+    password:             ENV['MAILTRAP_PASSWORD'],
+    domain:               ENV['MAILTRAP_DOMAIN'],
+    address:              ENV['MAILTRAP_SMTP_SERVER'],
+    port:                 ENV['MAILTRAP_SMTP_PORT'],
+    authentication:       'plain',
+    enable_starttls_auto: true }
+
+  config.action_mailer.default_url_options = { :host => "www.brainstormer.online" }
+  
+  config.action_mailer.default_options = {
+    from: '"Lone from Brainstormer" <lone@brainstormer.online>',
+    reply_to: "hello@brainstormer.online",
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
