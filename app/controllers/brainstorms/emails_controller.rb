1aaaf9a741156c391ae3fcb82498e57b5b1fd83c
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2024-12-27 23:10:46 +0100

    add email to paid tier

--- app/controllers/brainstorms/emails_controller.rb
+++ app/controllers/brainstorms/emails_controller.rb
@@ -2,18 +2,15 @@ class Brainstorms::EmailsController < ApplicationController
   include BrainstormScoped
 
   def create
-    @email = Brainstorm::Email.new
-    @email.email_address = params[:email_address]
+    @email_address = current_user.email
     respond_to do |format|
-      if @email.valid? 
-        IdeasMailer.with(token: params[:brainstorm_token], email_address: @email.email_address).ideas_email.deliver_later
-        flash.now[:success] = "Your email was successfully sent to #{@email.email_address}"
+      if @email_address
+        IdeasMailer.with(token: params[:brainstorm_token], email_address: @email_address).ideas_email.deliver_later
+        flash[:success] = "Your email was successfully sent to #{@email_address}"
         ahoy.track "Email sent successfully"
-        format.js
       else
         @email.errors.messages.each do |message|
-          flash.now[message.first] = message[1].first
-          format.js
+          flash[message.first] = message[1].first
         end
       end
     end
--- app/models/brainstorm/email.rb
+++ app/models/brainstorm/email.rb
@@ -3,6 +3,5 @@ class Brainstorm::Email
 
   attr_accessor :email_address
 
-  validates :email_address, presence: { message: "You need to type in your email." }
-  validates :email_address, format: { with: URI::MailTo::EMAIL_REGEXP, message: "That doesn't look like a valid email address." }
+  validates :email_address, presence: { message: "Oops! Something went wrong" }
 end
