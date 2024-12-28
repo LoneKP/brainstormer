e8b880f3aba1952b5a94e0b4f7583b8e2de224fd
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2024-12-28 20:37:46 +0100

    use flash.now

--- app/controllers/brainstorms/emails_controller.rb
+++ app/controllers/brainstorms/emails_controller.rb
@@ -6,11 +6,11 @@ class Brainstorms::EmailsController < ApplicationController
     respond_to do |format|
       if @email_address
         IdeasMailer.with(token: params[:brainstorm_token], email_address: @email_address).ideas_email.deliver_later
-        flash[:success] = "Your email was successfully sent to #{@email_address}"
+        flash.now[:success] = "Your email was successfully sent to #{@email_address}"
         ahoy.track "Email sent successfully"
       else
         @email.errors.messages.each do |message|
-          flash[message.first] = message[1].first
+          flash.now[message.first] = message[1].first
         end
       end
     end
